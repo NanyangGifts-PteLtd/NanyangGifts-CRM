f93f25d233484df0abdfa17c97123a09c65dc369
angel <avgelixe@gmail.com>
2026-07-17 17:36:51 +0800

    changed email redirect url to vercel site url

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -44,7 +44,7 @@ export function SignUpForm({
         email,
         password,
         options: {
-          emailRedirectTo: `${window.location.origin}/`,
+          emailRedirectTo: `https://nanyanggifts.vercel.app/auth/callback`,
         },
       });
       if (error) throw error;
